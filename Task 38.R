c0c484c663471f5f6ca93686e26ff4e5e1208b18
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-10-29 13:27:47 +0200

    Create Task 38.R

---
+++ Task 38.R
@@ -0,0 +1,8 @@
+ #Задача 38. Нека 𝑋 ∼ U(7, 9). Изобразете на графика плътността,
+ #функцията на разпределение и квантилната функция на 𝑋.
+ 
+ par(mfrow=c(1,3)) #matrica ot grafiki za da sa trite grafiki edna do druga v edin i sysh prozorec
+ curve(dunif(x,7,9),from=7,to=9)
+ curve(punif(x,7,9),,from=5,to=11)
+ curve(qunif(x,7,9),,from=0,to=1)
+ 
