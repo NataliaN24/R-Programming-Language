95ce79977043e74c9fb140897f1c5b02a1870e49
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-10-29 13:29:07 +0200

    Update Task 38.R

--- Task 38.R
+++ Task 38.R
@@ -2,7 +2,6 @@
  #функцията на разпределение и квантилната функция на 𝑋.
  
  par(mfrow=c(1,3)) #matrica ot grafiki za da sa trite grafiki edna do druga v edin i sysh prozorec
- curve(dunif(x,7,9),from=7,to=9)
- curve(punif(x,7,9),,from=5,to=11)
- curve(qunif(x,7,9),,from=0,to=1)
- 
+ curve(dunif(x,7,9),from=7,to=9) #za plytnostta
+ curve(punif(x,7,9),,from=5,to=11) # za funkciqta na razpredelenieto
+ curve(qunif(x,7,9),,from=0,to=1) #za kvantilnata funkciq
