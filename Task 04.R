e0d2430cb6b5bd652bae98aceac1806a2be372f3
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-10-05 19:20:40 +0300

    Update Task 04.R

--- Task 04.R
+++ Task 04.R
@@ -7,3 +7,31 @@ sum(x)==1
 sim.exam()
 res <- replicate(100000,sim.exam())
 sum(res)/length(res)
+
+
+
+#
+sim.exam <- function() {
+  questions <- c( rep(0,3), rep(1,17) )
+  x <- sample( questions, 2, replace=F )
+  sum(x)==1
+}
+
+prob.exam <- function(Nrep) {
+  res <- replicate( Nrep, sim.exam() )
+  sum(res)/length(res)
+}
+
+prob.exam(100000)
+
+
+Общо въпроси = 20
+
+Не знае = 3 (обозначаваме ги с 0)
+
+Знае = 17 (обозначаваме ги с 1)
+
+Тегли 2 въпроса без връщане
+
+Търсим вероятността да знае само един от двата
+
