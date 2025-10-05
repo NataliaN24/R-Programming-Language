f0d3d396bd096809a0a9f80efca44078c1b5ea44
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-10-05 19:30:53 +0300

    Update Task 06.R

--- Task 06.R
+++ Task 06.R
@@ -1,5 +1,23 @@
+#first solution
 x <-sample(c(1:20),20,replace=F)
 x
 d<-x-c(1:20)
 d
 any(d==0)
+
+#second solution
+
+sim.gifts<-function()
+{
+  x<-sample(c(1:20),20,replace=F)
+  d <- x- c(1,20)
+  any(d==0)
+}
+
+prob.gifts <-function(Nrep)
+{
+  res <-replicate(Nrep,sim.gifts())
+  sum(res)/length(res)
+}
+prob.gifts(1000000)
+
